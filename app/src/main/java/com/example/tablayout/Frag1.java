2cec2b3949a041534858bbea8f944525cf2d541a
taewoooh <taewoooh88@gmail.com>
2023-06-29 20:52:52 +0900

    ㅇㅇ

--- app/src/main/java/com/example/tablayout/Frag1.java
+++ app/src/main/java/com/example/tablayout/Frag1.java
@@ -1,21 +1,54 @@
 package com.example.tablayout;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 //외부에서 new Frag1 호출 시
-public class Frag1 extends Fragment {
+public class Frag1 extends Fragment{
+
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.frag1,container,false);
 
+
+        Log.e("체크해1",""+v);
+
         return v;
     }
+
+
 }
